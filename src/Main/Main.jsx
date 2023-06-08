56d63586bc5a9db82f3519143af69995e21df448
jakariachynabil10 <jakariachynabil@gmail.com>
2023-06-08 07:37:01 +0600

    footer is added

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import Navbar from '../Navbar/Navbar';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 
 const Main = () => {
-    return (
-        <div>
-            <div className='bg-[#000000bb] lg:px-10'>
-            <Navbar></Navbar>
-            </div>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <div className="bg-[#000000bb] lg:px-10">
+        <Navbar></Navbar>
+      </div>
+      <Outlet></Outlet>
+      <div className="mt-8">
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
